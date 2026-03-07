1aee70c71f212de31f767fc81cbd29fb8ff212ea
Nicolas Far <nicolas.far@renaissance.com>
2026-03-07 11:14:55 -0500

    Redis client before claude

--- src/lib.rs
+++ src/lib.rs
@@ -3,18 +3,52 @@
 use std::{thread::{self, sleep}, time::Duration};
 
 use ext_php_rs::prelude::*;
+use redis::{RedisError, TypedCommands as _};
 
-#[php_function]
-fn say_hello_from_thread(name: String) {
-    thread::spawn(move || {
-        for x in 0..5 {
-            php_println!("{} Hello, {}!", x,  name);
-            sleep(Duration::from_secs(1));
-        }
-    });
+struct RedisException {
+    inner: RedisError
 }
+
+// impl Into<PhpException> for RedisException {
+//     fn into(self) -> PhpException {
+//         PhpException::default(self.inner.detail().unwrap().to_string())
+//     }
+// }
+
+impl From<RedisException> for PhpException {
+    fn from(value: RedisException) -> Self {
+        //eprintln!("{:?}", value.inner.kind());
+        PhpException::default(value.inner.to_string())
+    }
+}
+
+impl From<RedisError> for RedisException {
+    fn from(value: RedisError) -> Self {
+        RedisException { inner: value }
+    }
+}
+
+
+#[php_class]
+struct RedisWrapper {
+    client: redis::Client,
+}
+
+
+#[php_impl]
+impl RedisWrapper {
+    fn __construct(server: &str) -> Result<Self, RedisException> {
+        let client = redis::Client::open(server)?;
+        Ok(Self { client })
+    }
+    
+    fn get_key(&mut self, key: &str) -> Result<Option<String>, RedisException> {
+        Ok(self.client.get(key)?)
+    }
+}
+
 
 #[php_module]
 pub fn get_module(module: ModuleBuilder) -> ModuleBuilder {
-    module.function(wrap_function!(say_hello_from_thread))
+    module.class::<RedisWrapper>()
 }
